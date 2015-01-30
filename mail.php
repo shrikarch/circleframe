8a58f1afa36c19fb11f3ba80e3b6e8f6e87a9289
shrikarch <shrikar.code@gmail.com>
2015-01-31 03:36:02 +0530

    486

--- mail.php
+++ mail.php
@@ -42,12 +42,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         date_default_timezone_set('Asia/Calcutta');
         $time = date("H:i:s Y-m-d");
 
-        $mail->Host = 'relay-hosting.secureserver.net';  // Specify main and backup SMTP servers
+        $mail->Host = 'localhost';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
         $mail->Username = 'info@circleframeproductions.com';                 // SMTP username
         $mail->Password = 'aniketvishal123';                           // SMTP password
-        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 465;                                    // TCP port to connect to
+        //$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 25;                                    // TCP port to connect to
 
         $mail->From = 'info@circleframeproductions.com';
         $mail->FromName = 'PC mail';
